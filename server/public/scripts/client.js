d13d496f5aa69d3f5e014329aaec031f5457ad6e
Manny Jo <jomansang@gmail.com>
2018-09-26 16:01:05 -0500

    add angular and angular route in client.js and index.html

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,9 @@
 console.log('client.js is loaded');
+
+const timeTrackerApp = angular.module('TimeTrackerApp', ['ngRoute']);
+
+timeTrackerApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {
+        template: '<h1>Hello</h1>'
+    })
+}]);
