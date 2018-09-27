adaebc31be847756772f9202cc3834c21f655142
Manny Jo <jomansang@gmail.com>
2018-09-27 09:40:06 -0500

    Add projects get connection between client and server

--- server/public/controllers/manage.controller.js
+++ server/public/controllers/manage.controller.js
@@ -1,8 +1,20 @@
-timeTrackerApp.controller('ManageController', [function(){
+timeTrackerApp.controller('ManageController', ['$http', function($http){
     let self = this;
 
     console.log('in ManageController');
 
     self.projectForm = {};
+
+    self.getProjectList = function(){
+        $http.get('/manage')
+            .then(function(response){
+                console.log('Response : ', response);
+            }).catch(function(error){
+                console.log('error:', error);
+                alert('Error with getting projects');
+            });
+    }
+
+    self.getProjectList();
 
 }]);
